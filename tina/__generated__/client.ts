42d456d95848c0e4fcd918f069ef87498b7c1124
Niranjan S.S <niranjanss2007@gmail.com>
2026-06-04 00:45:38 +0530

    style: remove signature SVG, container and unused store imports from Hero component

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: 'C:/Users/Niranjan S S/Desktop/Portfolio 2.0/tina/__generated__/.cache/1780513134371', url: 'https://content.tinajs.io/2.4/content/bb0d1782-9ec3-4e47-9aa9-2bed7187df28/github/main', token: '9b729aa66b21d3070ba33705bee04fc65ad32b43', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/Niranjan S S/Desktop/Portfolio 2.0/tina/__generated__/.cache/1780513957028', url: 'https://content.tinajs.io/2.4/content/bb0d1782-9ec3-4e47-9aa9-2bed7187df28/github/main', token: '9b729aa66b21d3070ba33705bee04fc65ad32b43', queries,  });
 export default client;
   
